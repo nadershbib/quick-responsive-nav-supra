cf1e9b1ca99cea43748b873a327712ed746f0ff1
Nader Shbib <nader_shbib@hotmail.com>
2020-12-14 21:56:39 -0800

    quick responsive nav

---
+++ app.js
@@ -0,0 +1,9 @@
+var burger = document.querySelector('.burger');
+var nav = document.querySelector('.nav-items')
+
+burger.addEventListener('click',()=>{
+   nav.classList.toggle('show-nav');
+})
+
+
+
